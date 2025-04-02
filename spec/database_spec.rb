e8ffdf50393f55232136782c928b6b5ee60062e3
equadras <emanueljose.quadras@gmail.com>
2025-04-02 13:26:08 -0300

    omg nao boto fe que funcionou

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,25 +1,36 @@
 require 'fileutils'
-
-RSpec.configure do |config|
-  config.before(:each) do
-    FileUtils.rm_f("test.db") # Remove o arquivo antes de cada teste
-  end
-end
-
 describe 'database' do
+  before do
+    `rm -rf test.db`
+  end
 
   def run_script(commands)
     raw_output = nil
-    IO.popen("./a.out test.db", "r+") do |pipe|
+    IO.popen("./db test.db", "r+") do |pipe|
       commands.each do |command|
         pipe.puts command
       end
 
       pipe.close_write
 
+      # Read entire output
       raw_output = pipe.gets(nil)
     end
     raw_output.split("\n")
+  end
+
+  it 'inserts and retreives a row' do
+    result = run_script([
+      "insert 1 user1 person1@example.com",
+      "select",
+      ".exit",
+    ])
+    expect(result).to eq([
+      "db > Executed.",
+      "db > (1, user1, person1@example.com)",
+      "Executed.",
+      "db > ",
+    ])
   end
 
   it 'keeps data after closing connection' do
@@ -27,7 +38,7 @@ describe 'database' do
       "insert 1 user1 person1@example.com",
       ".exit",
     ])
-    expect(result1).to match_array([
+    expect(result1).to eq([
       "db > Executed.",
       "db > ",
     ])
@@ -36,22 +47,8 @@ describe 'database' do
       "select",
       ".exit",
     ])
-    expect(result2).to match_array([
+    expect(result2).to eq([
       "db > (1, user1, person1@example.com)",
-      "Executed.",
-      "db > ",
-    ])
-  end
-
-  it 'inserts and retrieves a row' do
-    result = run_script([
-      "insert 1 user1 exemplo1@email.com",
-      "select",
-      ".exit",
-    ])
-    expect(result).to match_array([
-      "db > Executed.",
-      "db > (1, user1, exemplo1@email.com)",
       "Executed.",
       "db > ",
     ])
@@ -59,7 +56,7 @@ describe 'database' do
 
   it 'prints error message when table is full' do
     script = (1..1401).map do |i|
-      "insert #{i} user#{i} exemplo#{i}@email.com"
+      "insert #{i} user#{i} person#{i}@example.com"
     end
     script << ".exit"
     result = run_script(script)
@@ -75,10 +72,26 @@ describe 'database' do
       ".exit",
     ]
     result = run_script(script)
-    expect(result).to match_array([
+    expect(result).to eq([
       "db > Executed.",
       "db > (1, #{long_username}, #{long_email})",
       "Executed.",
+      "db > ",
+    ])
+  end
+
+  it 'prints error message if strings are too long' do
+    long_username = "a"*33
+    long_email = "a"*256
+    script = [
+      "insert 1 #{long_username} #{long_email}",
+      "select",
+      ".exit",
+    ]
+    result = run_script(script)
+    expect(result).to eq([
+      "db > String is too long.",
+      "db > Executed.",
       "db > ",
     ])
   end
@@ -90,13 +103,67 @@ describe 'database' do
       ".exit",
     ]
     result = run_script(script)
-    expect(result).to match_array([
+    expect(result).to eq([
       "db > ID must be a positive number",
       "db > Executed.",
       "db > ",
     ])
   end
 
+  it 'prints an error message if there is a duplicate id' do
+    script = [
+      "insert 1 user1 person1@example.com",
+      "insert 1 user1 person1@example.com",
+      "select",
+      ".exit",
+    ]
+    result = run_script(script)
+    expect(result).to eq([
+      "db > Executed.",
+      "db > Error: Duplicate key.",
+      "db > (1, user1, person1@example.com)",
+      "Executed.",
+      "db > ",
+    ])
+  end
 
+  it 'allows printing out the structure of a one-node btree' do
+    script = [3, 1, 2].map do |i|
+      "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << ".btree"
+    script << ".exit"
+    result = run_script(script)
+
+    expect(result).to eq([
+      "db > Executed.",
+      "db > Executed.",
+      "db > Executed.",
+      "db > Tree:",
+      "leaf (size 3)",
+      "  - 0 : 1",
+      "  - 1 : 2",
+      "  - 2 : 3",
+      "db > "
+    ])
+  end
+
+  it 'prints constants' do
+    script = [
+      ".constants",
+      ".exit",
+    ]
+    result = run_script(script)
+
+    expect(result).to eq([
+      "db > Constants:",
+      "ROW_SIZE: 293",
+      "COMMON_NODE_HEADER_SIZE: 6",
+      "LEAF_NODE_HEADER_SIZE: 10",
+      "LEAF_NODE_CELL_SIZE: 297",
+      "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+      "LEAF_NODE_MAX_CELLS: 13",
+      "db > ",
+    ])
+  end
 end
-
